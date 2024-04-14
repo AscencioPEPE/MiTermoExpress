e78bdc232fbf916edc2f00583b8815b229b71990
Zunirry <48844602+Zunirry@users.noreply.github.com>
2024-04-14 23:18:39 +0000

    Build: (dba42d8) style(Home): 💄 change styles home and added motion animation

--- _next/static/chunks/app/(products)/products/page-afc6b5878d731b7d.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[734],{2277:function(e,n,s){Promise.resolve().then(s.bind(s,8020))},8020:function(e,n,s){"use strict";s.r(n);var t=s(7437);n.default=()=>(0,t.jsx)("div",{className:"flex items-center justify-center"})}},function(e){e.O(0,[971,23,744],function(){return e(e.s=2277)}),_N_E=e.O()}]);
